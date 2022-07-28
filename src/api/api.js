91e361b006f12be8823865b43cd39e0d20bf4805
fengqianqian <2547617858@qq.com>
2022-07-28 19:38:21 +0800

    作业信息 P30分页组件封装

--- src/api/api.js
+++ src/api/api.js
@@ -1,12 +1,18 @@
 export function getData (root, url, params) {
+  console.log(params)
   root.service.get(url + '/count')
     .then(res => {
       // + '?limit' + root.total
       // eslint-disable-next-line standard/object-curly-even-spacing
+      res.status === 200 ? root.total = res.data : root.total = 0
       root.service.get(url, {params: params})
         .then(res => {
           if (res.status === 200) {
             console.log(res)
+            res.data.forEach(item => {
+              item.completion = item.completedNumber / (item.completedNumber + item.uncompletedNumber)
+              item.checkRate = 1 - item.check / (item.completedNumber + item.uncompletedNumber)
+            })
             root.tableData = [...res.data]
           } else {
             // ..
--- src/components/student/WorkList.vue
+++ src/components/student/WorkList.vue
@@ -45,7 +45,7 @@ export default {
       // 分页数据
       total: 100,
       currentPage: 1,
-      pageSize: 10,
+      pageSize: 5,
       formInline: {
         jobName: ''
       },
@@ -54,20 +54,27 @@ export default {
   },
   created () {
     let curr = (this.currentPage - 1) * this.pageSize
-    getData(this, '/works', {offset: curr, limit: this.pageSize})
+    getData(this, '/workList', {offset: curr, limit: this.pageSize})
   },
   methods: {
     // 分页函数
     hanldSizeChange (val) {
       this.pageSize = val
       this.currentPage = 1
-      getData(this, '/works', {offset: (this.currentPage - 1), limit: val})
+      getData(this, '/workList', {offset: (this.currentPage - 1), limit: val})
       console.log(val)
     },
     hanldCurrentChange (val) {
       this.currentPage = val
-      getData(this, '/works', {offset: (val - 1) * this.pageSize, limit: this.pageSize})
+      getData(this, '/workList', {offset: (val - 1) * this.pageSize, limit: this.pageSize})
       console.log(val)
+    },
+    find () {
+      getData(this, '/worList', this.formInline)
+    },
+    reset () {
+      getData(this, '/worList', {})
+      this.formInline = {}
     }
   }
 }
